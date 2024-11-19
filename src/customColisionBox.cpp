9db86908642114f6af87ef46964536cde1435dc3
Kikols <francisco.lapamsilva@gmail.com>
2024-11-19 20:44:34 +0000

    don't show collision boxes

--- src/customColisionBox.cpp
+++ src/customColisionBox.cpp
@@ -86,6 +86,7 @@ void customColisionBox::update() {
 
 
 void customColisionBox::draw3D() {
+    return;
     // draw the object in 2D
     //cout << minX << " " << minY << " " << minZ << " " << maxX << " " << maxY << " " << maxZ << endl;
     glPushMatrix();
